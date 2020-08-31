ca9b342db0eef4acb796d69eb064f5b029c2c38b
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-08-31 15:02:03 -0500

    begin working on bonus 2

---
+++ CarLotSimulator/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        //DONE - Create a seperate class file called Car
+        //DONE -Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        //DONE -Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        //DONE -The methods should take one string parameter: the respective noise property 
+        public Car()
+        {
+        }
+
+        public Car(int year,
+                   string make,
+                   string model,
+                   string engineNoise,
+                   string honkNoise,
+                   bool isDriveable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDriveable;
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine(EngineNoise);
+        }
+
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine(HonkNoise);
+        }
+    }
+}
--- CarLotSimulator/Program.cs
+++ CarLotSimulator/Program.cs
@@ -8,7 +8,7 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
+            //DONE - Create a seperate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property            
@@ -18,9 +18,47 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            //Instantiation 1 "Using Dot Notation"
+            var ryansCar = new Car();
+            ryansCar.Make = "Ford";
+            ryansCar.Model = "Focus";
+            ryansCar.Year = 2013;
+            ryansCar.EngineNoise = "VROOM";
+            ryansCar.HonkNoise = "LOUD";
+            ryansCar.IsDriveable = true;
+
+           
+            //Instantiation 2 "Object initializer syntax"
+            var missysCar = new Car()
+            {
+                Year = 1981,
+                Make = "Ford",
+                Model = "Bronco",
+                EngineNoise = "VERY LOUD",
+                HonkNoise = "NONE",
+                IsDriveable = true
+            };
+
+            //Instantiation 3 "using the constructer to allow parameter values to be placed inside properties"
+            //All parameters are required to use this way
+            var ryansBike = new Car(1994, "Harley-Davidson", "FXRP", "VERY LOUD", "NONE", true);
+
+
+            //Call Methods
+            ryansCar.MakeEngineNoise();
+
+            missysCar.MakeHonkNoise();
+
+            ryansBike.MakeEngineNoise();
+
+
+
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+
+            //DONE
 
             //*************BONUS X 2*************//
 
@@ -28,6 +66,10 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+
+
+
         }
     }
 }
